d3fdaccd69615c01d7cf2d885c7993a517489f4e
Arnaud <arnaud.tizon@institutoptique.fr>
2024-01-05 18:46:37 +0100

    add button to html result

--- example/live_w_locator.js
+++ example/live_w_locator.js
@@ -132,6 +132,7 @@ $(function() {
             });
         },
         _printCollectedResults: function() {
+//             TODO ca fait quoi exactement ?? WARNING j'ai modifé les h4 à la fin !!'
             var results = resultCollector.getResults(),
                 $ul = $("#result_strip ul.collector");
 
@@ -294,9 +295,9 @@ $(function() {
             App.lastResult = code;
             var $node = null, canvas = Quagga.canvas.dom.image;
 
-            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><button class="code"></button></div></div></li>');
             $node.find("img").attr("src", canvas.toDataURL());
-            $node.find("h4.code").html(code);
+            $node.find("button.code").html(code);
             $("#result_strip ul.thumbnails").prepend($node);
         }
     });
